ea192f5b91f94e8d8e954837983afbbdf6b3e513
VampireWeekend <32829088+VampireWeekend@users.noreply.github.com>
2017-10-16 14:42:07 +0800

    Update LCA-RMQ.cpp

--- LCA-RMQ.cpp
+++ LCA-RMQ.cpp
@@ -1,38 +1,38 @@
+int head[maxn],in[2*maxn],out[2*maxn],a[maxn*2],d[maxn*2];  
+int mn[maxn*2][20];
+
 //dfs序+RMQ实现LCA
-int head[maxn],a[maxn*2],mind[maxn*2][20],f[maxn],d[maxn],dep[maxn*2];
-bool visit[maxn];
-int dfn,num;
+void dfs(int now,int step) {  
+    visit[now]=1;  
+    a[++dfn]=now;d[dfn]=step;  
+    in[now]=out[now]=dfn; 
+    for (int i=head[now];i!=-1;i=edge[i].pre) {  
+        int to=edge[i].to;  
+        if (!visit[to]) {  
+            dfs(to,step+1);  
+            out[now]=++dfn;  
+            a[dfn]=now;d[dfn]=step;  
+        }  
+    }  
+}  
+  
+void init(int n) {  
+    int i,j;  
+    for (i=1;i<=n;i++) mn[i][0]=i;  
+    for (j=1;(1<<j)<=n;j++) {  
+        for (i=1;i+(1<<j)-1<=n;i++) {  
+            int p=mn[i][j-1],q=mn[i+(1<<(j-1))][j-1];  
+            mn[i][j]=d[p]<d[q]?p:q;  
+        }  
+    }  
+}  
+  
+int findlca(int l,int r) {  
+    int k=0;  
+    if (l>r) swap(l,r);  
+    while (1<<(k+1)<=(r-l+1)) k++;  
+    int p=mn[l][k],q=mn[r-(1<<k)+1][k];  
+    return d[p]<d[q]?a[p]:a[q];  
+}  
 
-void dfs(int now,int step) {
-	visit[now]=1;a[++dfn]=now;f[now]=dfn;dep[dfn]=step;
-	for (int i=head[now];i!=-1;i=edge[i].pre) {
-		int to=edge[i].to;
-		if (!visit[to]) {
-			d[to]=d[now]+1;
-			dfs(to,step+1);
-			a[++dfn]=now;
-			dep[dfn]=step;
-		}
-	}
-}
-
-void init(int n) {
-	int i,j;
-	for (i=1;i<=n;i++) mind[i][0]=i;
-	for (j=1;(1<<j)<=n;j++) {
-		for (i=1;i+(1<<j)-1<=n;i++) {
-			int p=mind[i][j-1],q=mind[i+(1<<(j-1))][j-1];
-			mind[i][j]=dep[p]<dep[q]?p:q;
-		}
-	}
-}
-//注意：查询x,y的LCA时应为find(f[x],f[y])
-int find(int l,int r) {
-	int k=0;
-	if (l>r) {
-		int p=l;l=r;r=p;
-	}
-	while (1<<(k+1)<=(r-l+1)) k++;
-	int p=mind[l][k],q=mind[r-(1<<k)+1][k];
-	return dep[p]<dep[q]?a[p]:a[q];
-}
+ lca=findlca(in[x],in[y]);  
