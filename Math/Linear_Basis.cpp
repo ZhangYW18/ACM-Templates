7221c1db5b180b153d56ded3675fd5423513b54e
VampireWeekend <32829088+VampireWeekend@users.noreply.github.com>
2017-11-24 21:18:10 +0800

    Update Linear_Basis.cpp

--- Math/Linear_Basis.cpp
+++ Math/Linear_Basis.cpp
@@ -3,6 +3,10 @@ bool linear_basis_insert(ll p) {
 		if ((p>>i)%2==1) {
 			if (!x[i]) {
 				x[i]=p;
+				for (int j=i-1;j>=0;j--) 
+					if (x[j]&&((x[i]>>j)%2==1)) x[i]^=x[j];
+                for (int j=i+1;j<=maxk;j++) 
+					if ((x[j]>>i)%2==1) x[j]^=x[i];
 				return true;
 			} else p=p^x[i];
 		}
