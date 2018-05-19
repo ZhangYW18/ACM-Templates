25fe3bb321c83db1a7833e39542ab1fa54f7f6fe
VampireWeekend <32829088+VampireWeekend@users.noreply.github.com>
2018-05-19 23:15:16 +0800

    Update SPFAwithSLF.cpp

--- Graphs/SPFAwithSLF.cpp
+++ Graphs/SPFAwithSLF.cpp
@@ -3,21 +3,17 @@ ll spfa(int s,int des){
     memset(inque,0,sizeof(inque));
     memset(dist,0x3f,sizeof(dist));
 	inque[s]=1;
-	deque<int> q;
+	que<int> q;
 	q.push_back(s);
 	dist[s]=0;
 	while (!q.empty()) {
 		int now=q.front();
-		q.pop_front();
+		q.pop();
 		inque[now]=0;
 		for (i=head[now];i!=-1;i=edge[i].pre) {
 			int to=edge[i].to;
 			if (dist[edge[i].from]+edge[i].dist<dist[to]) {
-				if (!q.empty()) {
-						if (dist[to]<dist[q.front()])  //small label first
-					        q.push_front(to);
-					    else q.push_back(to);
-					} else q.push_back(to);
+				q.push_back(to);
 				dist[to]=dist[edge[i].from]+edge[i].dist;
 			}
 		}
