416eb3ff08afdddfdfaac36cf88314965b030500
VampireWeekend <32829088+VampireWeekend@users.noreply.github.com>
2018-05-24 22:36:44 +0800

    Update Hash.cpp

--- Strings/Hash.cpp
+++ Strings/Hash.cpp
@@ -8,3 +8,15 @@ ull hash() {
     }  
     return sum;  
 }  
+
+//可截取区间的哈希
+ll hash() {
+    fac[0]=1;hash[0]=0;
+	for (int i=1;i<=n;i++) {
+		fac[i]=(fac[i-1]*inf)%mod;
+		hash[i]=(hash[i-1]*inf+(s[i]-'a')%mod;
+	}
+}
+ll gethash(int l,int r) {
+    return (((hash[r]-hash[l-1]*fac[r-l+1])%mod)+mod)%mod;
+}
