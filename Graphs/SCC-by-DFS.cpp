ffe3579bb1d700a41697146f758c93ecb5852993
VampireWeekend <32829088+VampireWeekend@users.noreply.github.com>
2018-07-20 19:28:20 +0800

    Update and rename SCC-by-DFS.cpp to Graphs/SCC-by-DFS.cpp

--- Graphs/SCC-by-DFS.cpp
+++ Graphs/SCC-by-DFS.cpp
@@ -5,13 +5,20 @@ void dfs1(int now) {
 	for (int i=0;i<size;i++) {
 		if (!vis[v[now][i]]) dfs1(v[now][i]);
 	}
-	q[++cnt]=now;
+	q[++num]=now;
 }
  
 void dfs2(int now) {
-	vis[now]=0;f[now]=c;
+	vis[now]=0;color[now]=c;
 	int size=v1[now].size();
 	for (int i=0;i<size;i++) {
 		if (vis[v1[now][i]]) dfs2(v1[now][i]);
 	}
 }
+
+
+	num=c=0;
+	for (i=1;i<=num;i++) 
+		if (!vis[i]) dfs1(i);
+	for (i=num;i;i--) 
+		if (vis[q[i]]) c++,dfs2(q[i]);
