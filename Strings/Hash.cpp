c62a6e4551083389ad6d8c3a12f5f5aa88f376f7
VampireWeekend <32829088+VampireWeekend@users.noreply.github.com>
2018-07-26 15:27:54 +0800

    Update Hash.cpp

--- Strings/Hash.cpp
+++ Strings/Hash.cpp
@@ -10,13 +10,20 @@ ull hash() {
 }  
 
 //可截取区间的哈希
-ll hash() {
-    fac[0]=1;hash[0]=0;
+ull fac[maxn],hsh[maxn],fac2[maxn],hsh2[maxn];
+unordered_map<ll,int> mp,mp2;
+const ull seed=100001651;
+void myhash(int n) {
+    fac[0]=fac2[0]=1;hsh[0]=hsh2[0]=0;
 	for (int i=1;i<=n;i++) {
-		fac[i]=(fac[i-1]*inf)%mod;
-		hash[i]=(hash[i-1]*inf+(s[i]-'a')%mod;
+		fac[i]=(fac[i-1]*seed);
+		hsh[i]=hsh[i-1]*seed+s[i];
+		fac2[i]=(fac2[i-1]*seed2)%mod2;
+		hsh2[i]=hsh2[i-1]*seed2%mod2+s[i];
+		hsh2[i]%=mod2;
 	}
 }
+
 ll gethash(int l,int r) {
     return (((hash[r]-hash[l-1]*fac[r-l+1])%mod)+mod)%mod;
 }
