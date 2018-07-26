255d03c56057b44d16ffd6b6124b718864741f5b
VampireWeekend <32829088+VampireWeekend@users.noreply.github.com>
2018-07-26 15:30:29 +0800

    Update Hash.cpp

--- Strings/Hash.cpp
+++ Strings/Hash.cpp
@@ -12,15 +12,14 @@ ull hash() {
 //可截取区间的哈希
 ull fac[maxn],hsh[maxn],fac2[maxn],hsh2[maxn];
 unordered_map<ll,int> mp,mp2;
-const ull seed=100001651;
+const ull seed=100001651,seed2=63218479;
 void myhash(int n) {
     fac[0]=fac2[0]=1;hsh[0]=hsh2[0]=0;
 	for (int i=1;i<=n;i++) {
-		fac[i]=(fac[i-1]*seed);
+		fac[i]=fac[i-1]*seed;
 		hsh[i]=hsh[i-1]*seed+s[i];
-		fac2[i]=(fac2[i-1]*seed2)%mod2;
-		hsh2[i]=hsh2[i-1]*seed2%mod2+s[i];
-		hsh2[i]%=mod2;
+		fac2[i]=fac2[i-1]*seed2;
+		hsh2[i]=hsh2[i-1]*seed2+s[i];
 	}
 }
 
