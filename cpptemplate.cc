7523133f0e9062955709b2a8b4f80bdb5d5a7554
ZhangYW18 <yiweizh18@gmail.com>
2022-12-19 14:22:58 -0500

    add cpp template

---
+++ cpptemplate.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define pb push_back
+#define mem0(a) memset(a, 0, sizeof(a))
+#define meminf(a) memset(a, 0x3f, sizeof(a))
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef double db;
+typedef pair<int,int> pii;
+const int maxn = 100005, inf = 0x3f3f3f3f;
+const ll llinf = 0x3f3f3f3f3f3f3f3f;
+const ld pi = acos(-1.0L);
+
+int main() {
+    
+    return 0;
+}
